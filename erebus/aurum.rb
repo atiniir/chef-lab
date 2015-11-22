3b05ccffa9e79d82697594d4acda19e75a78cb2a
Kristan Stewart <kristan.stewart@gmail.com>
2015-11-21 23:54:15 -0800

    fix options, machine is generated!

--- erebus/aurum.rb
+++ erebus/aurum.rb
@@ -1,6 +1,18 @@
 require 'chef/provisioning'
 
+options = {
+  vagrant_options: {
+  	'vm.box' => 'bento/centos-6.7',
+#  	'vm.ram' => '1G',
+#	'vm.cpus' => '1',
+    'vm.hostname' => 'lab-wrkstn',
+    'vm.network' => '"private_network", ip: "192.168.10.18"'
+  },
+}
+
 machine 'aurum' do
   tag 'gold'
+  machine_options options
   converge true
 end
+
--- erebus/vagrant_linux.rb
+++ erebus/vagrant_linux.rb
@@ -6,6 +6,14 @@ require 'chef/provisioning/vagrant_driver'
 
 with_driver 'vagrant'
 
-with_machine_options :vagrant_options => {
-  'vm.box' => 'bento/centos-6.7'
-}
+#with_machine_options :vagrant_options => {
+#  'vm.box' => 'bento/centos-6.7',
+#  'vm.ram' => '1G',
+#  'vm.cpus' => '1'
+#}
+
+with_chef_local_server :chef_repo_path => '~/Lab/chef-repo'
+
+#with_chef_server "https://chef-server.example.org",
+#  :client_name => Chef::Config[:node_name],
+#  :signing_key_filename => Chef::Config[:client_key]
