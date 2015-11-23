28c4c4b80fe1fe348862288cfa7c11e569f8d779
Kristan Stewart <kristan.stewart@gmail.com>
2015-11-23 14:49:19 -0800

    initial add of kris cookbook

---
+++ cookbooks/kris/metadata.rb
@@ -0,0 +1,7 @@
+name             'Kristan Stewart'
+maintainer       'Kristan Stewart'
+maintainer_email 'kristan.stewart@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures kris'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ cookbooks/kris/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: kris
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+file '~/hello.txt' do
+	  content 'Welcome to Chef'
+end
