f7c5b1815afaf63e897a47020f45adba08e2be6e
Kristan Stewart <kristan.stewart@gmail.com>
2015-11-23 17:19:03 -0800

    more recipes for kris

---
+++ cookbooks/kris/recipes/ci.rb
@@ -0,0 +1,15 @@
+
+# install chef server and jenkins 
+
+
+#    {
+#      "chef-server": {
+#      "configuration": "notification_email 'kstewart@ea.com'",
+#      "addons": [
+#         "manage"
+#       ]
+#       },
+#       "run_list":[
+#       "recipe[chef-server::default]","recipe[chef-server::addons]"
+#       ]
+#    }
