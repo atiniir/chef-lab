da39c9472d88d92f4c1b9c516292c5b1fe4669bc
Kristan Stewart <kristan.stewart@gmail.com>
2015-11-25 15:14:37 -0800

    cleanup and testing

--- cookbooks/kris/attributes/default.rb
+++ cookbooks/kris/attributes/default.rb
@@ -1,6 +1,17 @@
+####################
+# => CHEF-DK 
+####################
 default['chef-dk']['global_shell_init'] = "false"
+
+####################
+# => CHEF-SERVER 
+####################
 default['chef-server']['configuration'] = ['notification_email','kristan.stewart@gmail.com']
 default['chef-server']['addons'] = ['manage']
+
+####################
+# => JAVA 
+####################
 default['java']['install_flavor'] = "oracle"
 default['java']['jdk_version'] = "8"
 default['java']['oracle']['accept_oracle_download_terms'] = "true"
--- cookbooks/kris/recipes/ci.rb
+++ cookbooks/kris/recipes/ci.rb
@@ -1,19 +1,19 @@
 
 # install chef server and jenkins 
-include_recipe 'java'
-include_recipe 'jenkins::master'
-include_recipe 'chef-server'
+#include_recipe 'java'
+#include_recipe 'jenkins::master'
+include_recipe 'chef-server::default'
 include_recipe 'chef-server::addons'
 #include_recipe 'git'
 
 
 # KS TODO should really load the plugin list from another file or something
 
-jenkins_plugin 'log-parser'
-jenkins_plugin 'git'
+#jenkins_plugin 'log-parser'
+#jenkins_plugin 'git'
 
 # restart so plugins are found 
-jenkins_command 'safe-restart'
+#jenkins_command 'safe-restart'
 
 # KS TODO - add credential setup  basic and LDAP 
 
