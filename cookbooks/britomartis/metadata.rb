04cc3845013c4f2cbd898aa7ce1c89d00b30f3b0
Kristan <kstewart@ea.com>
2015-11-26 13:26:42 -0800

    update from template

--- cookbooks/britomartis/metadata.rb
+++ cookbooks/britomartis/metadata.rb
@@ -1,7 +1,13 @@
+#####
+#
+# => Exercise Kitchen against vRA and Vagrant
+#
+#####
+
 name 'britomartis'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Kristan Stewart'
+maintainer_email 'kristan.stewart@gmail.com'
 license 'all_rights'
-description 'Installs/Configures britomartis'
-long_description 'Installs/Configures britomartis'
+description 'Basic cookbook to see how kitchen workflows work with Vagrant and vRA'
 version '0.1.0'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
