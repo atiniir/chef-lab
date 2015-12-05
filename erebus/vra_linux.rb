9b15200d809ef161fbb02729b96eb90f98cc1c31
Kristan Stewart <kristan.stewart@gmail.com>
2015-12-04 17:31:27 -0800

    make example settings easier to use and diff

--- erebus/vra_linux.rb
+++ erebus/vra_linux.rb
@@ -1,9 +1,10 @@
 
+#with_driver 'vra:https://vraapp.example.com'
 with_driver 'vra:https://vraapp.example.com'
-
 
 with_chef_local_server :chef_repo_path => '~/Desktop/Lab/chef-repo'
 
-#with_chef_server "https://chef-server.example.org",
-#  :client_name => Chef::Config[:node_name],
-#  :signing_key_filename => Chef::Config[:client_key]
+#with_chef_server "https://chefserver.example.com",
+with_chef_server "https://chefserver.example.com",
+  :client_name => Chef::Config[:node_name],
+  :signing_key_filename => Chef::Config[:client_key]
