aba6c8ea0a1a62f64852b9efee71706fb3ad3637
Kristan Stewart <kristan.stewart@gmail.com>
2015-12-04 17:29:14 -0800

    fix cookbook path, add template for using server

--- erebus/vra_linux.rb
+++ erebus/vra_linux.rb
@@ -1,3 +1,9 @@
-#require 'chef/provisioning/vra_driver'
 
-with_driver 'vra:https://vraapp.example.com' 
+with_driver 'vra:https://vraapp.example.com'
+
+
+with_chef_local_server :chef_repo_path => '~/Desktop/Lab/chef-repo'
+
+#with_chef_server "https://chef-server.example.org",
+#  :client_name => Chef::Config[:node_name],
+#  :signing_key_filename => Chef::Config[:client_key]
